17fe8f9cf673abaffe8527838fb0d636c10b53a4
Elbe Somes <elbesomes@192-168-1-105.tpgi.com.au>
2023-07-01 22:59:53 +1000

    adding location to creating rg

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -16,6 +16,6 @@ provider "azurerm" {
 
 # Create Resource Group 
 resource "azurerm_resource_group" "my_demo_rg1" {
-  location = "eastus"
-  name = "my-demo-rg1"  
+  location = "eastusaustraliasoutheast"
+  name = "elbe-demo-rg1"  
 }
